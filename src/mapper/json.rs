d713ec335e7a6752988fcf2d0d3c6c29d9a86ae8
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-03-23 12:49:16 +0100

    update depdendencies and bump to 0.1.3

--- src/mapper/json.rs
+++ src/mapper/json.rs
@@ -5,7 +5,7 @@ use serde_json::Value;
 pub fn get_one(doc: &Value, path: &str) -> Option<Value> {
   let selector = Selector::new(path).unwrap();
 
-  let results: Vec<&Value> = selector.find_all(&doc);
+  let results: Vec<&Value> = selector.find(&doc).collect();
 
   if results.len() != 1 {
     return None;
