e7194deed3ac41fda0bae80363827c34f5892ac9
Marc-Antoine Arnaud <arnaud.marcantoine@gmail.com>
2018-08-30 11:12:13 +0200

    remove files with the same name as their module

--- src/definition/mod.rs
+++ src/definition/mod.rs
@@ -6,3 +6,15 @@ pub mod rdf;
 pub mod rdfs;
 pub mod undefined;
 pub mod xsi;
+
+pub trait Definition {
+    fn get_namespace(&self) -> &str;
+
+    fn has_property(&self, property: &str) -> bool;
+
+    fn get_label(&self, l: &str) -> String {
+        let mut field = String::from(Self::get_namespace(self));
+        field.push_str(l);
+        field
+    }
+}
